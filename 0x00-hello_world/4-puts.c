a334cceed8fd6528dad40cafb9539e36debb8daf
proabudu <abuduprosperyao@gmail.com>
2023-06-15 15:40:18 -0700

    try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-/* my view */
-int main(void)
-{
-puts("Programming is like building a multilingual puzzle");
-return: 0 (sucess)
+
+int main(void) {
+  puts("Programming is like building a multilingual puzzle\n");
+  return 0;
 }
