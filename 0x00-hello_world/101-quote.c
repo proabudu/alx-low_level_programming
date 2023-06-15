1a23ef5ec1ec6c842714c92a470b3b3fb7277604
proabudu <abuduprosperyao@gmail.com>
2023-06-15 16:33:49 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+    /* Use fprintf to print to the standard error */
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+    /* Return 1 to indicate error */
+    return (1);
+}
+
