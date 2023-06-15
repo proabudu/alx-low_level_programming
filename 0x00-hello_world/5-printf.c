179ebeb9ba9c4ceb895567674464853ee4e33048
proabudu <abuduprosperyao@gmail.com>
2023-06-15 15:45:09 -0700

    F

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,5 @@
 
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n"); return (0);
 }
