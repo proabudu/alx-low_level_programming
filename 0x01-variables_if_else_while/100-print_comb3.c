0d077f59b88c242ef96ba2358b168e1ef90b6a1a
proabudu <abuduprosperyao@gmail.com>
2023-06-16 17:13:31 -0700

    Final Commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: 0 if successful
+ */
+int main(void)
+{
+    int i, j;
+
+    i = 0;
+    while (i < 10)
+    {
+        j = i + 1;
+        while (j < 10)
+        {
+            putchar(i + '0');
+            putchar(j + '0');
+            if (i < 8 || j < 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+            j++;
+        }
+        i++;
+    }
+    putchar('\n');
+    return (0);
+}
+
