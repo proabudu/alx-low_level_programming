f3ba57a3edfb8309211b0d17bafeb82bc7522d2a
proabudu <abuduprosperyao@gmail.com>
2023-06-16 17:03:52 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+
+    i = 0;
+    while (i <= 99)
+    {
+        j = i + 1;
+        while (j <= 99)
+        {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (i < 98 || j < 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+            j++;
+        }
+        i++;
+    }
+    putchar('\n');
+    return (0);
+}
+
