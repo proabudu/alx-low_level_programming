451d81d676bfecd2615783d621ba401b83495a88
proabudu <abuduprosperyao@gmail.com>
2023-06-20 14:29:42 -0700

    Greatly Done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
     _putchar('_');
@@ -16,7 +17,6 @@ int main(void)
     _putchar('a');
     _putchar('r');
     _putchar('\n');
-
     return (0);
 }
 
