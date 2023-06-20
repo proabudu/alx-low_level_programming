4526dcc72064cb44deadf26c5d91b16567450dea
proabudu <abuduprosperyao@gmail.com>
2023-06-20 13:38:33 -0700

    TRY

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * Return: Always
+ */
 
 int main(void)
 {
-    printf("_putchar\n");
-    return (0);
-}
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
+    return 0;
+}
