2dd722452fdf911779ce74b26b64215a5d73ccd8
proabudu <abuduprosperyao@gmail.com>
2023-06-20 11:41:37 -0700

    all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,7 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-int main(void)
-{
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+int _putchar(char c);
 
-    return (0);
-}
-
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
+#endif /* MAIN_H */
 
