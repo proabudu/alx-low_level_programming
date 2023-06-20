15d7fa8ee744159cfd576a636cd87f9e367f3201
proabudu <abuduprosperyao@gmail.com>
2023-06-20 15:49:16 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,13 @@
  */
 int print_last_digit(int n)
 {
-    int last;
-
-    last = n % 10;
-    if (last < 0)
-    {
-        last = -last;
-    }
-    _putchar(last + '0');
-    return (last);
+int last;
+last = n % 10;
+if (last < 0)
+{
+last = -last;
+}
+_putchar(last + '0');
+return (last);
 }
 
