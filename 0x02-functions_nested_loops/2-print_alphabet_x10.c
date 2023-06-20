258d60000359826f18a090295e81818e73d04a8f
proabudu <abuduprosperyao@gmail.com>
2023-06-20 14:59:01 -0700

    F

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,31 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char letter;
+for (i = 0; i < 10; i++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+}
+_putchar('\n');
+}
 
 /**
- * main - check the code for Holberton School students.
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
  *
- * Return: Always 0.
+ * Return: On success, 1. On error, -1 is returned and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	print_alphabet_x10();
-	return (0);
+/* Replace the code below with your own implementation of _putchar */
+/* You can use any method you prefer to print a single character */
+return (write(1, &c, 1));
 }
+
