9ad51ea8c96577fb960082d5e217a2054f1e74e0
proabudu <abuduprosperyao@gmail.com>
2023-06-20 16:34:21 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,21 +7,21 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n < 98; n++)
-        {
-            printf("%d, ", n);
-        }
-        printf("98\n");
-    }
-    else
-    {
-        for (; n > 98; n--)
-        {
-            printf("%d, ", n);
-        }
-        printf("98\n");
-    }
+if (n <= 98)
+{
+for (; n < 98; n++)
+{
+printf("%d, ", n);
+}
+printf("98\n");
+}
+else
+{
+for (; n > 98; n--)
+{
+printf("%d, ", n);
+}
+printf("98\n");
+}
 }
 
