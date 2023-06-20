17e627c076621fa38b8f3a3083320fcbdddce031
proabudu <abuduprosperyao@gmail.com>
2023-06-20 16:23:46 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,23 @@
 #include <iostream>
-
 void print_to_98(int n) {
-    while (n != 99) {
-        std::cout << n;
-        if (n != 98) {
-            std::cout << ", ";
-        }
-        if (n < 98) {
-            n++;
-        } else {
-            n--;
-        }
-    }
-    std::cout << std::endl;
+while (n != 99) 
+std::cout << n;
+if (n != 98) {
+std::cout << ", ";
+}
+if (n < 98) {
+n++;
+} else {
+n--;
+}
+}
+std::cout << std::endl;
+}
+int main() {
+int number;
+std::cout << "Enter a number: ";
+std::cin >> number;
+print_to_98(number);
+return 0;
 }
 
