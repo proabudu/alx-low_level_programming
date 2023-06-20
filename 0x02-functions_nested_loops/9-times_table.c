9c3746df49f6b32b7402fc5ce66d7819fc73103e
proabudu <abuduprosperyao@gmail.com>
2023-06-20 16:03:14 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,35 +5,34 @@
  */
 void times_table(void)
 {
-    int i;
-    int j;
-    int product;
-
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            product = i * j;
-            if (j == 0)
-            {
-                _putchar('0');
-            }
-            else if (product < 10)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-        }
-        _putchar('\n');
-    }
+int i;
+int j;
+int product;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+product = i * j;
+if (j == 0)
+{
+_putchar('0');
+}
+else if (product < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(product + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
 
