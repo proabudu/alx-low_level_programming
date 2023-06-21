f96e36c55bbf29e6e4bffb395282bb1416ac08a7
proabudu <abuduprosperyao@gmail.com>
2023-06-20 18:25:46 -0700

    @ERROR@

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,26 +1,32 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_times_table - prints the n times table, starting with 0
- * @n: the times table to print
- */
+void _putchar(char c);
+
 void print_times_table(int n) {
-  if (n < 0 || n > 15) {
-    return;
-  }
+    if (n < 0 || n > 15) {
+        return;
+    }
 
-  for (int i = 0; i <= 15; i++) {
-    int product = i * n;
-    _putchar('0' + i);
-    _putchar(' ');
-    _putchar('*');
-    _putchar(' ');
-    _putchar('0' + n);
-    _putchar(' ');
-    _putchar('=');
-    _putchar(' ');
-    _putchar('0' + product);
-    _putchar('\n');
-  }
+    int row, col;
+
+    for (row = 0; row <= n; row++) {
+        for (col = 0; col <= n; col++) {
+            int result = row * col;
+
+            // Print tens digit if non-zero
+            if (result >= 10) {
+                _putchar((result / 10) + '0');
+            }
+
+            _putchar((result % 10) + '0');  // Print units digit
+
+            // Print comma and space unless it's the last column
+            if (col != n) {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
 }
 
