faa08b85bb4ad176c594a9d8611cce5dc263b5f3
proabudu <abuduprosperyao@gmail.com>
2023-06-20 17:58:32 -0700

    100-main.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,22 +19,32 @@ void print_times_table(int n)
                 product = i * j;
                 if (j == 0)
                 {
-                    printf("%d", product);
+                    _putchar(product + '0');
                 }
                 else if (product < 10)
                 {
-                    printf(",   %d", product);
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar(product + '0');
                 }
                 else if (product < 100)
                 {
-                    printf(",  %d", product);
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar((product / 10) + '0');
+                    _putchar((product % 10) + '0');
                 }
                 else
                 {
-                    printf(", %d", product);
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar((product / 100) + '0');
+                    _putchar(((product / 10) % 10) + '0');
+                    _putchar((product % 10) + '0');
                 }
             }
-            printf("\n");
+            _putchar('\n');
         }
     }
 }
