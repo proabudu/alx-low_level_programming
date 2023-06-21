2911f39f2861e5f502119640dd8df3033769e05f
proabudu <abuduprosperyao@gmail.com>
2023-06-20 18:01:59 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,25 @@
-#include "main.h"
+void _putchar(char c);
 
-/**
- * print_times_table - prints the n times table, starting with 0
- * @n: the times table to print
- */
-void print_times_table(int n)
-{
-    int i;
-    int j;
-    int product;
+void print_times_table(int n) {
+    if (n < 0 || n > 15) {
+        return;
+    }
 
-    if (n >= 0 && n <= 15)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                product = i * j;
-                if (j == 0)
-                {
-                    _putchar(product + '0');
-                }
-                else if (product < 10)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(product + '0');
-                }
-                else if (product < 100)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar((product / 10) + '0');
-                    _putchar((product % 10) + '0');
-                }
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar((product / 100) + '0');
-                    _putchar(((product / 10) % 10) + '0');
-                    _putchar((product % 10) + '0');
-                }
+    int row, col;
+
+    for (row = 0; row <= n; row++) {
+        for (col = 0; col <= n; col++) {
+            int result = row * col;
+            if (result < 10) {
+                _putchar(result + '0');
+            } else {
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
             }
-            _putchar('\n');
+            _putchar(',');
+            _putchar(' ');
         }
+        _putchar('\n');
     }
 }
 
