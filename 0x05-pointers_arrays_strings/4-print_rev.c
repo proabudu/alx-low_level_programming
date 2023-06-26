2a19c87543afc2982209d0febc5120f5dc56e85a
proabudu <abuduprosperyao@gmail.com>
2023-06-26 00:54:56 -0700

    gross

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,8 @@ void print_rev(char *s)
 {
 int length = 0;
 int i;
-// Calculate the length of the string
 while (s[length] != '\0')
 length++;
-// Print the string in reverse
 for (i = length - 1; i >= 0; i--)
 write(1, &s[i], 1);
 write(1, "\n", 1);
