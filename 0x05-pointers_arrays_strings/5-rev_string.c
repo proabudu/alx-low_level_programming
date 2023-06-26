9e37d2e43e15b9e6b896c9e9cd3e7bb32de61d24
proabudu <abuduprosperyao@gmail.com>
2023-06-26 01:39:59 -0700

    try

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,7 @@
-void rev_string(char *s)
-{
-int length = 0;
-char temp;
-while (s[length] != '\0')
-{
-length++;
-}
-for (int i = 0, j = length - 1; i < j; i++, j--)
-{
-temp = s[i];
+void rev_string(char *s) {
+int i, j;
+for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+char temp = s[i];
 s[i] = s[j];
 s[j] = temp;
 }
