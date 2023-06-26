e0833f786ef7e49487c2da8e393284dbce508fb9
proabudu <abuduprosperyao@gmail.com>
2023-06-26 03:04:59 -0700

    DS

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,26 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * main - generates random valid passwords for the program 101-crackme.
- *
- * Return: Always 0.
- */
-int main(void)
+#define PASSWORD_LENGTH 8
+char *generate_password()
 {
-int i;
-int sum;
-char c;
-srand(time(NULL));
-while (sum < 2772)
+static const char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
+if (password == NULL)
 {
-c = rand() % 127;
-if (c < 33)
-continue;
-putchar(c);
-sum += c;
+fprintf(stderr, "Memory allocation failed.\n");
+exit(1);
 }
-putchar(2772 - sum);
-return (0);
+srand(time(NULL));
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+int random_index = rand() % (sizeof(characters) - 1);
+password[i] = characters[random_index];
+}
+password[PASSWORD_LENGTH] = '\0';
+return password;
+}
+int main()
+{
+char *password = generate_password();
+printf("Generated password: %s\n", password);
+free(password);
+return 0;
 }
 
