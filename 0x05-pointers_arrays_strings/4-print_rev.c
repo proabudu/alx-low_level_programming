6da1869eeeadfedd0b10d12fe26aeb0ad179c26a
proabudu <abuduprosperyao@gmail.com>
2023-06-26 01:04:20 -0700

    Betty style error correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 /**
  * print_rev - prints a string in reverse followed by a new line
  * @s: pointer to the string to be printed
- * Description: This function takes a pointer to a string as a parameter
+ * This function takes a pointer to a string as a parameter
  * and prints the string in reverse followed by a new line to the standard output
  */
 void print_rev(char *s)
