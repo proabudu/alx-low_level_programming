dfa7272711a9e6aad79ba27fbe65bcb07e459d15
proabudu <abuduprosperyao@gmail.com>
2023-08-02 13:06:26 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include "main"
+
 int _strlen_recursion(char *s)
 {
-/* If we've rd the end of the string, return 0 */
+/* If we've reached the end of the string, return 0 */
 if (*s == '\0')
-return (0);
-/* Recursively call the function with the next character and add 1 to the result */
+return 0;
 return (_strlen_recursion(s + 1) + 1);
 }
 
