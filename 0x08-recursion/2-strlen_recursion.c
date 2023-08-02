17ca9c1556f4d0a390829f26d41dd39650e67f20
proabudu <abuduprosperyao@gmail.com>
2023-08-02 13:09:35 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,8 @@ int _strlen_recursion(char *s)
 {
 /* If we've reached the end of the string, return 0 */
 if (*s == '\0')
-return 0;
+return (0);
+/* Recursively call the function with the next character and add 1 to the result */
 return (_strlen_recursion(s + 1) + 1);
 }
 
