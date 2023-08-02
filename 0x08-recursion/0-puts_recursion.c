8228cebddb589385cc48de840ea4492e2c7f854c
proabudu <abuduprosperyao@gmail.com>
2023-08-02 12:22:16 -0700

    DF

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+/* If we've reached the end of the string, print a newline and return
+ * Print the current character and move on to the next one
+ */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
