69f4e9038d0f2a6b029ad60a794072246628d02e
proabudu <abuduprosperyao@gmail.com>
2023-08-02 10:49:24 -0700

    d

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-/**
- * main - check the code
- * For NULL definition
- * Return: Always 0.
- */
+// main - check the code
+// For NULL definition
+// Return: Always 0.
+ 
 int main(void)
 {
     _puts_recursion("Puts with recursion");
