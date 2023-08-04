13a4cdf2063df954508d2e6e19fec9ff460228e0
proabudu <abuduprosperyao@gmail.com>
2023-08-04 13:16:09 -0700

    all task done

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#indef MAIN_H
+#define MAIN_H
+
+int _putchar(char c).
+int atoi(char "s);
+
+#endif
