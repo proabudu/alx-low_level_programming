be4dcf95d08b40ba8f8af1de56c3624361e2e2fa
proabudu <abuduprosperyao@gmail.com>
2023-08-09 16:54:07 -0700

    dssd

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,46 +1,41 @@
 #include "main.h"
 #include <stdlib.h>
-
-/* This function concatenates all the arguments of the program */
-/* It takes two parameters: ac (argument count) and av (argument vector) */
-/* It returns a pointer to a new string, or NULL if it fails */
-/* Each argument is followed by a \n in the new string */
-
+/**
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
+ */
 char *argstostr(int ac, char **av)
 {
-char *str; /* pointer to the new string */
-int i, j, k; /* loop variables */
-int len; /* length of the new string */
-/* check if ac or av is invalid */
-if (ac == 0 || av == NULL)
-return (NULL);
-/* calculate the length of the new string */
-len = 0;
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-len++;
-len++; /* for the \n character */
-}
-len++; /* for the \0 character */
-/* allocate memory for the new string */
-str = malloc(sizeof(char) * len);
-if (str == NULL)
-return (NULL);
-/* copy each argument to the new string */
-k = 0;
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-{
-str[k] = av[i][j];
-k++;
-}
-str[k] = '\n';
-k++;
-}
-str[k] = '\0';
-/* return the pointer to the new string */
-return (str);
+	int i, n, r = 0, l = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			l++;
+	}
+	l += ac;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	}
+	return (str);
 }
 
