11d984712468261872066ee3dae02cd2f3c1d621
proabudu <abuduprosperyao@gmail.com>
2023-08-09 14:36:57 -0700

    try to correct Betty style error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 
 /**
-* create_array - Creates an array of the specified size and fills it with the
-* specified value.
-*
-* @param size The size of the array to create.
-* @param value The value to fill the array with.
-*
-* @return A pointer to the created array, or NULL on failure).
+*create_array - Creates an array of the specified size and fills it with the
+*specified value.
+*@param size The size of the array to create.
+*@param value The value to fill the array with.
+*@return A pointer to the created array, or NULL on failure).
 */
 char *create_array(unsigned int size, char c)
 {
@@ -22,5 +20,5 @@ return (NULL);
 for (i = 0; i < size; i++)
 array[i] = c;
 return (array);
-} 
+}
 
