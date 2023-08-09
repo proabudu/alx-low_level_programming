ff9c2dce94414d538028b52353734d240fd7715e
proabudu <abuduprosperyao@gmail.com>
2023-08-09 14:31:56 -0700

    try to correct Betty style error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,17 @@
 *
 * @return A pointer to the created array, or NULL on failure).
 */
-
-char *create_array(unsigned int size, char c) {
-if (size == 0) {
-return NULL;
-}
-char *array = malloc(size * sizeof(char));
-if (array == NULL) {
-return NULL;
-}
-for (unsigned int i = 0; i < size; i++) {
+char *create_array(unsigned int size, char c)
+{
+char *array;
+unsigned int i;
+if (size == 0)
+return (NULL);
+array = malloc(size * sizeof(char));
+if (array == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
 array[i] = c;
-}
-return array;
-}
+return (array);
+} 
 
