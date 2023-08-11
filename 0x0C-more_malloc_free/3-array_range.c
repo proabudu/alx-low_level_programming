a5dca797de053587c242f6ca8f29861c6f3b5d15
proabudu <abuduprosperyao@gmail.com>
2023-08-11 00:58:24 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,19 +9,15 @@
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int i;
-
-    if (min > max)
-        return (NULL);
-
-    arr = malloc(sizeof(int) * (max - min + 1));
-    if (arr == NULL)
-        return (NULL);
-
-    for (i = 0; min <= max; i++, min++)
-        arr[i] = min;
-
-    return (arr);
+int *arr;
+int i;
+if (min > max)
+return (NULL);
+arr = malloc(sizeof(int) * (max - min + 1));
+if (arr == NULL)
+return (NULL);
+for (i = 0; min <= max; i++, min++)
+arr[i] = min;
+return (arr);
 }
 
