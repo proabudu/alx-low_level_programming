3a362c89caa1d1bb9ea52ed6fb914b653db3905e
proabudu <abuduprosperyao@gmail.com>
2023-08-11 16:05:30 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,53 +1,36 @@
-include <stdlib.h>
+#include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  // Check the number of arguments
-  if (argc != 3) {
-    printf("Error\n");
-    exit(98);
-  }
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 98 on error
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
 
-  // Check if num1 and num2 are only composed of digits
-  for (int i = 0; i < strlen(argv[1]); i++) {
-    if (argv[1][i] < '0' || argv[1][i] > '9') {
-      printf("Error\n");
-      exit(98);
+    if (argc != 3)
+    {
+        printf("Error\n");
+        exit(98);
     }
-  }
 
-  for (int i = 0; i < strlen(argv[2]); i++) {
-    if (argv[2][i] < '0' || argv[2][i] > '9') {
-      printf("Error\n");
-      exit(98);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+
+    if (num1 < 0 || num2 < 0)
+    {
+        printf("Error\n");
+        exit(98);
     }
-  }
 
-  // Calculate the product
-  int product = 0;
-  for (int i = 0; i < strlen(argv[1]); i++) {
-    int carry = 0;
-    for (int j = 0; j < strlen(argv[2]); j++) {
-      int digit1 = argv[1][i] - '0';
-      int digit2 = argv[2][j] - '0';
-      int productDigit = digit1 * digit2 + carry;
-      carry = productDigit / 10;
-      productDigit %= 10;
-      product = product * 10 + productDigit;
-    }
-    if (carry > 0) {
-      product = product * 10 + carry;
-    }
-  }
+    result = num1 * num2;
+    printf("%d\n", result);
 
-  // Print the result
-  for (int i = 0; i < strlen(argv[1]) + strlen(argv[2]); i++) {
-    _putchar(product % 10 + '0');
-    product /= 10;
-  }
-
-  _putchar('\n');
-
-  return 0;
+    return (0);
 }
 
