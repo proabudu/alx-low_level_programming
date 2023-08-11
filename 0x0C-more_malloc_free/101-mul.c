750e389f3c44b7e5660dc256567d34eec756aa38
proabudu <abuduprosperyao@gmail.com>
2023-08-11 15:43:32 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,108 +1,140 @@
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
-int _putchar(char c);
 /**
- * is_digit - checks if a character is a digit
- * @c: character to check
+ * _strlen - returns the length of a string.
+ * @str: string to get the length of.
  *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: length of the string.
  */
-int is_digit(char c)
+unsigned int _strlen(char *str)
 {
-    return (c >= '0' && c <= '9');
+	unsigned int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
 }
+
 /**
- * is_number - checks if a string is a number
- * @s: string to check
+ * _isdigit - checks if a character is a digit.
+ * @c: character to check.
  *
- * Return: 1 if s is a number, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise.
  */
-int is_number(char *s)
+int _isdigit(char c)
 {
-while (*s)
-{
-if (!is_digit(*s))
-return (0);
-s++;
+	return (c >= '0' && c <= '9');
 }
-return (1);
-}
+
 /**
- * _atoi - converts a string to an integer
- * @s: string to convert
+ * _puts - prints a string to stdout.
+ * @str: string to print.
+ */
+void _puts(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
+
+/**
+ * mul - multiplies two positive numbers.
+ * @num1: first number to multiply.
+ * @num2: second number to multiply.
  *
- * Return: integer value of the string
+ * Return: pointer to the result or NULL on failure.
  */
-int _atoi(char *s)
+char *mul(char *num1, char *num2)
 {
-int sign = 1;
-unsigned int num = 0;
-while (*s)
-{
-if (*s == '-')
-sign *= -1;
-else if (is_digit(*s))
-num = num * 10 + (*s - '0');
-else if (num > 0)
-break;
-s++;
+	char *result;
+	unsigned int len1, len2, i, j, k, carry = 0;
+
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+
+	result = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1 + len2; i++)
+		result[i] = '0';
+
+	result[i] = '\0';
+
+	for (i = len1 - 1; i < len1; i--)
+	{
+		if (!_isdigit(num1[i]))
+			return (NULL);
+
+		carry = 0;
+
+		for (j = len2 - 1, k = i + j + 1; j < len2; j--, k--)
+		{
+			if (!_isdigit(num2[j]))
+				return (NULL);
+
+			carry += (num1[i] - '0') * (num2[j] - '0') + result[k] - '0';
+			result[k] = carry % 10 + '0';
+			carry /= 10;
+		}
+
+		result[k] += carry;
+	}
+
+	while (*result == '0' && *(result + 1) != '\0')
+		result++;
+
+	return (result);
 }
-return (sign * num);
-}
+
 /**
- * print_number - prints an integer
- * @n: integer to print
+ * main - multiplies two positive numbers.
+ * @argc: number of arguments passed to the program.
+ * @argv: array of arguments passed to the program.
+ *
+ * Return: 0 on success, 98 on failure.
  */
-void print_number(int n)
-{
-unsigned int num;
-if (n < 0)
-{
-_putchar('-');
-num = -n;
-}
-else
-num = n;
-if (num / 10)
-print_number(num / 10);
-_putchar((num % 10) + '0');
-}
-/**
-* main - multiplies two positive numbers
-* @argc: number of arguments
-* @argv: array of arguments
-*
-* Return: 0 on success, 98 on error
-*/
 int main(int argc, char **argv)
 {
-int num1, num2, result;
-if (argc != 3)
-{
-_putchar('E');
-_putchar('r');
-_putchar('r');
-_putchar('o');
-_putchar('r');
-_putchar('\n');
-exit(98);
-}
-if (!is_number(argv[1]) || !is_number(argv[2]))
-{
-_putchar('E');
-_putchar('r');
-_putchar('r');
-_putchar('o');
-_putchar('r');
-_putchar('\n');
-exit(98);
-}
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
-result = num1 * num2;
-print_number(result);
-_putchar('\n');
-return (0);
+	char *result;
+
+	if (argc != 3)
+	{
+		_puts("Error\n");
+		exit(98);
+	}
+
+	if (*argv[1] == '\0' || *argv[2] == '\0')
+	{
+		_puts("Error\n");
+		exit(98);
+	}
+
+	result = mul(argv[1], argv[2]);
+
+	if (result == NULL)
+	{
+		_puts("Error\n");
+		exit(98);
+	}
+
+	if (*result == '\0')
+		result--;
+
+	while (*result != '\0')
+	{
+		_putchar(*result);
+		result++;
+	}
+
+	_putchar('\n');
+
+	free(result);
+
+	return (0);
 }
 
