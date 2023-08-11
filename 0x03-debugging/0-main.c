516fdb68b2711df5fff8cd7aec0c55974f1f56e3
proabudu <abuduprosperyao@gmail.com>
2023-08-11 16:46:19 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
+ * main - checks if a number is positive or negative
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
 {
-int i;
-i = 98;
-positive_or_negative(i);
+int num;
+if (argc != 2)
+{
+printf("Usage: %s <num>\n", argv[0]);
+return (1);
+}
+num = atoi(argv[1]);
+if (num > 0)
+printf("%d is positive\n", num);
+else if (num < 0)
+printf("%d is negative\n", num);
+else
+printf("%d is zero\n", num);
 return (0);
 }
+
