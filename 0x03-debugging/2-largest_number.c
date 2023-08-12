d25c8e6c9b0d3f3f9149c59839924b4cbb467693
proabudu <abuduprosperyao@gmail.com>
2023-08-11 17:14:14 -0700

    de

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,31 +1,30 @@
-#include <stdio.h>
+#include "main.h"
 
-int largest_number(int *array, int size) {
-  int max = array[0];
-  int max2 = INT_MIN;
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 
-  for (int i = 1; i < size; i++) {
-    if (array[i] > max) {
-      max2 = max;
-      max = array[i];
-    } else if (array[i] > max2) {
-      max2 = array[i];
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a >= b && a >= c)
+    {
+        largest = a;
     }
-  }
+    else if (b >= a && b >= c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
 
-  return max;
-}
-
-int main() {
-  int array[] = {10, 1, 2};
-  int size = sizeof(array) / sizeof(array[0]);
-
-  int largest = largest_number(array, size);
-  int second_largest = max2;
-
-  printf("The largest number is %d.\n", largest);
-  printf("The second largest number is %d.\n", second_largest);
-
-  return 0;
+    return (largest);
 }
 
