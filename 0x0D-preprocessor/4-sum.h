9b269af5137ad27bbc7f38a340fe47b963860b27
proabudu <abuduprosperyao@gmail.com>
2023-08-14 05:48:34 -0700

    4-main.c

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,8 @@
-#define SUM(x, y) ((x) + (y))
+#ifndef SUM_H
+#define SUM_H
 
 /* Define a function-like macro SUM(x, y) that computes the sum of x and y */
+#define SUM(x, y) ((x) + (y))
+
+#endif
 
