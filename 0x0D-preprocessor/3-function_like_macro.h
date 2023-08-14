5804759089513775ea0eb4afe66a969fdb664748
proabudu <abuduprosperyao@gmail.com>
2023-08-14 05:53:12 -0700

    all

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,8 @@
+#ifndef FUNCTION_LIKE_MACRO_H
 #include <stdio.h>
+
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
 /* Define a function-like macro ABS(x) that computes the absolute value of x */
 
+#endif
