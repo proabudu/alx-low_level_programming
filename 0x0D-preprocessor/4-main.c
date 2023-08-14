56fbfa7058b4db85f41d490fdaa145e0fc221a44
proabudu <abuduprosperyao@gmail.com>
2023-08-14 05:42:26 -0700

    4-main.c

--- 0x0D-preprocessor/4-main.c
+++ 0x0D-preprocessor/4-main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "4-sum.h"
-#include "4-sum.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 int s;
