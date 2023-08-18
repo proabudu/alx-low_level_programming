f0de7363c77b6558f3ace30685c6a560a8f4c9e2
proabudu <abuduprosperyao@gmail.com>
2023-08-17 17:47:21 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include "main.h"
 
 
@@ -17,17 +16,10 @@
  * Return: Always 0 (Success)
 
  */
-
-
 int main(int argc, char *argv[])
-
 {
-
 (void) argv; /*Ignore argv*/
-
-        printf("%d\n", argc - 1);
-
-        return (0);
-
+printf("%d\n", argc - 1);
+return (0);
 }
 
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #include "main.h"
-
 
 /**
 
@@ -16,45 +14,23 @@
  */
 
 int _atoi(char *s)
-
 {
-
-        int i, d, n, len, f, digit;
-
-
-        i = 0;
-
-        d = 0;
-
-        n = 0;
-
-        len = 0;
-
-        f = 0;
-
-        digit = 0;
-
-
-        while (s[len] != '\0')
-
-                len++;
-
-
-
-        while (i < len && f == 0)
-
-        {
-
-                if (s[i] == '-')
-
-                        ++d;
-
-
-                if (s[i] >= '0' && s[i] <= '9')
-
-                {
-
-                        digit = s[i] - '0';
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
+{
+if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
 
                         if (d % 2)
 
