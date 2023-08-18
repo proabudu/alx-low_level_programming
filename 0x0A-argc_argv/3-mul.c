7b8a258aaf2a444acc66f500c322263e74f23449
proabudu <abuduprosperyao@gmail.com>
2023-08-18 01:18:23 +0000

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -39,13 +39,6 @@ if (f == 0)
 return (0);
 return (n);
 }
-/**
-* main - multiplies two numbers
-* @argc: number of arguments
-* @argv: array of arguments
-*
-* Return: 0 (Success), 1 (Error)
-*/
 int main(int argc, char *argv[])
 {
 int result, num1, num2;
